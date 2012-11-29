55d170d95e71eb3ee4c69e75c670b5acae957c10
Mark Lacomber <marklacomber@gmail.com>
2012-11-29 16:42:37 +0000

    Added setup.py to use git as pip endpoint

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='LogentriesLogger',
+	version='0.2',
+	author='Mark Lacomber',
+	author_email='marklacomber@gmail.com',
+	packages=['logentries'],
+	scripts=[],
+	url='http://pypi.python.org/pypi/LogentriesLogger/',
+	license='LICENSE.txt',
+	description='Python Logger plugin to send logs to Logentries',
+	long_description=open('README.txt').read(),
+)
