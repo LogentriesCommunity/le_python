97282ef2e401dc08b19c028cc997110e3427ab01
Mark Lacomber <marklacomber@gmail.com>
2013-11-28 16:14:08 +0000

    Added compatiblity for python3 and moved some helpers methods into separat efile

--- logentries/utils.py
+++ logentries/utils.py
@@ -1,7 +1,12 @@
-VERSION = '2.0.1'
+
+""" This file contains some utils for connecting to Logentries
+    as well as storing logs in a queue and sending them."""
+
+VERSION = '2.0.2'
+
+from logentries import helpers as le_helpers
 
 import logging
-import Queue
 import threading
 import socket
 import random
@@ -21,114 +26,107 @@ MAX_DELAY = 10
 LE = "LE: "
 # Error message displayed when an incorrect Token has been detected
 INVALID_TOKEN = "\n\nIt appears the LOGENTRIES_TOKEN parameter you entered is incorrect!\n\n"
+# Unicode Line separator character   \u2028
+LINE_SEP = le_helpers.to_unicode('\u2028')
+
 
 def dbg(msg):
     print LE + msg
 
-def check_token(token):
-    import re
-
-    valid = re.compile(r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
-	
-    return valid.match(token)
 
 class SocketAppender(threading.Thread):
     def __init__(self):
-	    threading.Thread.__init__(self)
-	    self.daemon = True
-	    self._conn = None
-	    self._queue = Queue.Queue(QUEUE_SIZE)
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self._conn = None
+        self._queue = le_helpers.create_queue(QUEUE_SIZE)
 
     def openConnection(self):
-	    self._conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
-	    self._conn.connect((LE_API, LE_PORT))
+        self._conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._conn.connect((LE_API, LE_PORT))
 
     def reopenConnection(self):
-	    self.closeConnection()
+        self.closeConnection()
 
-	    root_delay = MIN_DELAY
-	    while True:
-		    try:
-			    self.openConnection()
-			    return
-		    except Exception, e:
-			    dbg("Unable to connect to Logentries")
+        root_delay = MIN_DELAY
+        while True:
+            try:
+                self.openConnection()
+                return
+            except Exception:
+                dbg("Unable to connect to Logentries")
 
-		    root_delay *= 2
-		    if(root_delay > MAX_DELAY):
-			    root_delay = MAX_DELAY
-		
-		    wait_for = root_delay + random.uniform(0, root_delay)
-		    
-		    try:
-			    time.sleep(wait_for)
-		    except KeyboardInterrupt, e:
-				raise KeyboardInterrupt
-	
+            root_delay *= 2
+            if(root_delay > MAX_DELAY):
+                root_delay = MAX_DELAY
+
+            wait_for = root_delay + random.uniform(0, root_delay)
+
+            try:
+                time.sleep(wait_for)
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt
+
     def closeConnection(self):
-	    if(self._conn != None):
-		    self._conn.close()
-
+        if self._conn is not None:
+            self._conn.close()
 
     def run(self):
-	    try:
-		    # Open connection
-		    self.reopenConnection()
+        try:
+            # Open connection
+            self.reopenConnection()
 
-		    # Send data in queue
-		    while True:
-			    # Take data from queue
-				data = self._queue.get(block=True)
-				
-				# Replace newlines with Unicode line separator for multi-line events
-				if not isinstance(data, unicode):
-					multiline = unicode(data, "utf-8").replace('\n', u'\u2028')
-				else:
-					multiline = data.replace('\n', u'\u2028')
-				multiline += "\n"
-			    # Send data, reconnect if needed
-				while True:
-				    try:
-					    self._conn.send(multiline.encode("utf-8"))	
-				    except socket.error, e:
-					    self.reopenConnection()
-					    continue
-						
-				    break
-	    except KeyboardInterrupt, e:
-		    dbg("Logentries asynchronous socket client interrupted")
+            # Send data in queue
+            while True:
+                # Take data from queue
+                data = self._queue.get(block=True)
 
-	    self.closeConnection()
+                # Replace newlines with Unicode line separator for multi-line events
+                if not le_helpers.is_unicode(data):
+                    multiline = le_helpers.create_unicode(data).replace('\n', LINE_SEP)
+                else:
+                    multiline = data.replace('\n', LINE_SEP)
+                multiline += "\n"
+                # Send data, reconnect if needed
+                while True:
+                    try:
+                        self._conn.send(multiline.encode('utf-8'))
+                    except socket.error:
+                        self.reopenConnection()
+                        continue
+                    break
+        except KeyboardInterrupt:
+            dbg("Logentries asynchronous socket client interrupted")
+
+        self.closeConnection()
 
 
 class LogentriesHandler(logging.Handler):
     def __init__(self, token):
-	    logging.Handler.__init__(self)
-	    self.token = token
-	    self.good_config = True
-	    if not check_token(token):
-		    dbg(INVALID_TOKEN)
-		    self.good_config = False
-	    format = logging.Formatter('%(asctime)s : %(levelname)s, %(message)s', '%a %b %d %H:%M:%S %Z %Y')
-	    self.setFormatter(format)
-	    self.setLevel(logging.DEBUG)
-	    self._thread = SocketAppender()
-	    
+        logging.Handler.__init__(self)
+        self.token = token
+        self.good_config = True
+        if not le_helpers.check_token(token):
+            dbg(INVALID_TOKEN)
+            self.good_config = False
+        format = logging.Formatter('%(asctime)s : %(levelname)s, %(message)s', '%a %b %d %H:%M:%S %Z %Y')
+        self.setFormatter(format)
+        self.setLevel(logging.DEBUG)
+        self._thread = SocketAppender()
+
     @property
     def _started(self):
-    	return self._thread.is_alive()
+        return self._thread.is_alive()
 
     def emit(self, record):
+        if not self._started and self.good_config:
+            dbg("Starting Logentries Asynchronous Socket Appender")
+            self._thread.start()
 
-	    if not self._started and self.good_config:
-		    dbg("Starting Logentries Asynchronous Socket Appender") 
-		    self._thread.start()
+        msg = self.format(record).rstrip('\n')
+        msg = self.token + msg
 
-	    msg = self.format(record).rstrip('\n')
-	    msg = self.token + msg
-
-	    self._thread._queue.put(msg)
-
+        self._thread._queue.put(msg)
 
     def close(self):
-	    logging.Handler.close(self)
+        logging.Handler.close(self)
