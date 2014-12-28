a41e66fef81c8d1b7586b461ee32f3540ea19a49
Thomas Grainger <tom@yplanapp.com>
2014-12-28 17:41:49 +0000

    support TLS

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
 
 setup(
-	name='LogentriesLogger',
-	version='0.2',
-	author='Mark Lacomber',
-	author_email='marklacomber@gmail.com',
-	packages=['logentries'],
-	scripts=[],
-	url='http://pypi.python.org/pypi/LogentriesLogger/',
-	license='LICENSE.txt',
-	description='Python Logger plugin to send logs to Logentries',
-	long_description=open('README.rst').read(),
+    name='LogentriesLogger',
+    version='0.2',
+    author='Mark Lacomber',
+    author_email='marklacomber@gmail.com',
+    packages=['logentries'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/LogentriesLogger/',
+    license='LICENSE.txt',
+    description='Python Logger plugin to send logs to Logentries',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "certifi",
+    ],
 )
