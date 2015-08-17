a10865bcd5265ea41472cad68f01c21f55a9982f
Stephen Hynes <stephen.hynes@logentries.com>
2015-08-17 14:38:14 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Logentries',
-    version='0.7',
+    version='0.8',
     author='Mark Lacomber',
     author_email='marklacomber@gmail.com',
     packages=['logentries'],
